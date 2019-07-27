5dfbcab0a10da6db06462ab8e3b0eb299d62a716
federicokakilalaflores <federicokakilalaflores@gmail.com>
2019-07-27 21:27:09 +1200

    modify the main js code structure

--- main.js
+++ main.js
@@ -1,78 +1,66 @@
 //initialize
+let calculator = document.getElementById("calculator");
 let btns  = document.getElementById("btns");
 let display  = document.getElementById("calcEntry");
 const numbers =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const operators = ["+", "-", "*", "/", "**"];
 let equation = "";
-let currentEntry = "";
-let entryLen = currentEntry.length; 
-let res = "";
   
 btns.addEventListener("click", e => {
-    clearFunction(e.target.id);
+    clearFunction(e.target);
     calculationProcess(e.target);
 });
 
-// All clear key functionality
 function clearFunction(key){
-    function clearAll(){
-        currentEntry = "";
-        entryLen = 0;
+    if(key.id == "ca"){
+        display.innerText = "0";
         equation = "";
     }
 
-    function backspace(){
-        (entryLen > 0) ? entryLen-- : entryLen = 0;
-        currentEntry = currentEntry.substring(0, entryLen);
-    }
-
-    (key == "ca") ? clearAll() : false;
-    (key == "ce") ? backspace() : false;
+    if(key.id == "ce"){
+        if(display.innerText.length == 1){
+            display.innerText = "0";
+        }else{
+            display.innerText = display.innerText.slice(0, -1);
+        }    
+    }    
 }
 
-function displays(){
-    display.innerText = currentEntry.substring(0, entryLen);
-    console.log("equation " + equation);
-    console.log("entry " + currentEntry);
-    console.log(res);
-}
-
-// the process of calculation
 function calculationProcess(key){
-    let initialRes = "" 
+    const keyValue = key.id; 
+    const displayNum = display.innerText;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    let result = "";
 
-    if(entryLen >= 15){ 
-        currentEntry += "";
-    }else if(operators.includes(key.id) === true){
-        equation += currentEntry + key.id;
-        initialRes = eval(equation.substring(0, equation.length-1)).toString();
-        currentEntry = initialRes;
-        entryLen = initialRes.length;
+    if(numbers.includes(keyValue)){
+        calculator.dataset.previousKeyType = "number";
+        if(keyValue === "0" && displayNum == 0)
+        {
+            display.innerText = keyValue;
+        }else{
+            if(displayNum === "0" || previousKeyType == "operator"){ 
+                (keyValue == ".") ? display.innerText = "0" + keyValue :  display.innerText = keyValue;
+            }else{
+                    display.innerText = displayNum + keyValue;     
+            } 
+        }   
+    }else if(operators.includes(keyValue)){
+        if(displayNum != "0"){
+            equation += displayNum + keyValue;
+            (keyValue == "**") ? result = eval(equation.slice(0, -2)) : result = eval(equation.slice(0, -1)); 
+            display.innerText = result; 
 
-        //add pressed state for an operator
-        key.classList.add("is-pressed");
-        
-    }else if(numbers.includes(key.id) === true){
-        if(operators.includes(equation[equation.length - 1])){
-            currentEntry = "";
-            entryLen = 0; 
+            calculator.dataset.previousKeyType = "operator";
         }
-
-        currentEntry += key.id;
-        entryLen++;      
-
-        //remove pressed btn state and back to its normal form
-        Array.from(key.parentNode.children).forEach(k => k.classList.remove("is-pressed"));
-
     }else if(key.id == "="){
-        res = eval(equation + currentEntry).toString();
-        currentEntry = res;
-        entryLen = res.length;
+        display.innerText = eval(equation + displayNum); 
         equation = "";
     }
 
-    displays();
+    console.log(equation); 
+
 }
+
 
 
 
